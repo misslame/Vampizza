beaf5308e9a9c6aed909f3b698611d0cdae3c034
misslame <14coxkm@gmail.com>
2021-05-27 18:37:11 -0700

    Add Camera zoom with maxium/minim limits

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -6,11 +6,15 @@ public class CameraControler : MonoBehaviour{
     public float PAN_MIN_X = 20;
     public float PAN_MAX_X = 20;
 
-    public float PAN_MIN_Y = 10;
+    public float PAN_MIN_Y = 13;
     public float PAN_MAX_Y = 10;
+
+    public int PAN_MIN_Z = -15;
+    public int  PAN_MAX_Z = -5;
 
     public float panSpeed = 20f;
     public float panBorder = 10f;
+    public int scrollSpeed = 2;
 
     Vector3 pos;
 
@@ -43,8 +47,22 @@ public class CameraControler : MonoBehaviour{
             pos.x += panSpeed * Time.deltaTime;
         }
 
+        if (Input.GetKey(KeyCode.Plus)){
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Minus)){
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        int scroll = (int)(Input.GetAxis("Mouse ScrollWheel") * 10);
+        pos.z += scroll * scrollSpeed;
+
+
         pos.x = Mathf.Clamp(pos.x, -PAN_MIN_X, PAN_MAX_X);
         pos.y = Mathf.Clamp(pos.y, -PAN_MIN_Y, PAN_MAX_Y);
+        pos.z = Mathf.Clamp(pos.z, PAN_MIN_Z, PAN_MAX_Z);
+
 
         transform.position = pos;
 
