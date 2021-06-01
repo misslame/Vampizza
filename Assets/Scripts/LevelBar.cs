f700d03689d487ae6b1bc6b3424be550c92fdcc7
misslame <14coxkm@gmail.com>
2021-06-01 16:13:15 -0700

    implemented ui for a level bar

---
+++ Assets/Scripts/LevelBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelBar : MonoBehaviour {
+
+    public Slider levelSlider;
+    
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update() {}
+
+    // Sets the experience needed before leveling up to the passed in float value
+    public void SetExperienceNeeded(float experience) {
+        levelSlider.maxValue = experience;
+        levelSlider.value = 0;
+        
+    }
+
+    // Sets the current value for experience to the passed in float value
+    public void SetCurrentExperience(float experience) {
+        levelSlider.value = experience;
+    }
+    
+    // Retrieves current experience progress. 
+    public float GetCurrentProgressExperience() {
+        return levelSlider.value;
+    }
+
+}
---
+++ Assets/Scripts/LevelDisplay.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDisplay : MonoBehaviour {
+
+    public int level = 1; // TEMPORARY TILL BETTER SOLUTION
+
+    public Text levelText;
+    public LevelBar levelBar;
+
+    // Start is called before the first frame update
+    void Start() {
+        levelBar.SetExperienceNeeded(50f);
+    }
+
+    // Update is called once per frame **** TEMPORARY *****
+    void Update() {
+
+        levelText.text = level.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            levelBar.SetCurrentExperience(levelBar.GetCurrentProgressExperience() + 1f);
+
+            if (levelBar.GetCurrentProgressExperience() >= (level * 50f)) {
+                level++;
+                levelBar.SetExperienceNeeded(50f * level);
+            }
+        }
+
+    }
+
+    // Sets the display to the current level. 
+    void setLevelText(string level) {
+        levelText.text = level.ToString()
+    }
+}
