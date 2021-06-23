182f2d5d211c92d2bf622ff11209470089b687cb
misslame <14coxkm@gmail.com>
2021-06-22 18:36:21 -0700

    revert to 305552d63ee7c50fbca6bfd3bd9935fda045d4cd  big oof

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,39 +7,13 @@ public class Player : MonoBehaviour {
     //CONSTANTS
     const float EXP_MODIFIER = 50f;
 
-    //PRIVATES
-    private PlayerData data;
-
     //PLAYER ATTRIBUTES
-    public float exp {
-        get { return data.exp; }
-        set { data.exp = value; }
-    }
-
-    public ulong level {
-        get { return data.level; }
-        set { data.level = value; }
-    }
-
-    public long currency {
-        get { return data.currency; }
-        set { data.currency = value; }
-    }
-
-    public string playerName {
-        get { return data.playerName; }
-        set { data.playerName = value; }
-    }
-
-    public ulong citizen {
-        get { return data.citizen; }
-        set { data.citizen = value; }
-    }
-
-    public ulong blood {
-        get { return data.blood; }
-        set { data.blood = value; }
-    }
+    public float exp;
+    public ulong level;
+    public long currency;
+    public string playerName;
+    public ulong citizen;
+    public ulong blood;
 
     //UI ELEMENT REFERENCES
     public LevelBar levelBar;
@@ -50,8 +24,6 @@ public class Player : MonoBehaviour {
     //CUSTOM CONTRUCTOR
 
     public Player(){
-        data = GameState.GetPlayerData();
-
         exp = 0f;
         level = 1;
         currency = 0;
--- Assets/Scripts/Save/Data/SerializableData.cs
+++
@@ -1,6 +0,0 @@
-using System.IO;
-
-public abstract class SerializableData {
-    public abstract void Serialize(BinaryWriter bw);
-    public abstract void Deserialize(BinaryReader br);
-}
