9881bc3af384d539addc8461e7d69d5472651ea8
jamsge <james.ge1@outlook.com>
2021-08-08 14:49:54 -0700

    Initial CreateStructure code, which should be identical to how structures are currently placed.
    - Implemented invalid tiles, which are placed when a structure type is unknown
    - made many methods static because that's how they should be

--- Assets/Scripts/StructureTileHandler.cs
+++ Assets/Scripts/StructureTileHandler.cs
@@ -5,22 +5,39 @@ using UnityEngine.Tilemaps;
 
 public class StructureTileHandler : MonoBehaviour
 {
-    Tilemap TilemapStructures;
+    static Tilemap TilemapStructures;
     [SerializeField] Tile TownHomeTile;
     [SerializeField] Tile HomeTile;
     [SerializeField] Tile FarmPlotTile;
+    [SerializeField] Tile InvalidTile;
 
-    public Dictionary<Vector3Int, StructureData> Data;
+    private static Dictionary<Vector3Int, StructureData> StructureData;
+    private static Dictionary<string, Tile> TileDictionary;
+    private static Inventory PlayerInventory;
 
     void Start(){
         TilemapStructures = GameObject.FindWithTag("Structures").GetComponent<Tilemap>();
+        TileDictionary = new Dictionary<string, Tile>()
+        {
+            {"Home", HomeTile},
+            {"TownHome", HomeTile},
+            {"FarmPlot", FarmPlotTile},
+            {"Invalid", InvalidTile}
+        };
+        PlayerInventory = GameObject.FindWithTag("Inventory").GetComponent<Inventory>();
     }
 
-    static void CreateStructure(Vector3Int coord, Structure structure){
-
+    public static void CreateStructure(Vector3Int coord, Structure structure){
+        if (!TileDictionary.ContainsKey(structure.GetType().ToString())){
+            Debug.LogError("Stucture type not found!");
+            TilemapStructures.SetTile(coord, TileDictionary["Invalid"]);
+            return;
+        }
+        PlayerInventory.structureQuantities[structure.GetType().ToString()]--;
+        TilemapStructures.SetTile(coord, TileDictionary[structure.GetType().ToString()]);
     }
 
-    static bool PutAwayStructure(Vector3Int coord){
+    public static bool PutAwayStructure(Vector3Int coord){
         return false;
     }
 }
