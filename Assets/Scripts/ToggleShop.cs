92374907274db1746dcd6b0433d2e007a82920a6
jamsge <james.ge1@outlook.com>
2021-08-10 20:14:03 -0700

    Added shop toggle cooldown timer

--- Assets/Scripts/ToggleShop.cs
+++ Assets/Scripts/ToggleShop.cs
@@ -7,12 +7,20 @@ public class ToggleShop : MonoBehaviour
 {
     [SerializeField] GameObject InventoryGameObj;
     Toggle shopToggle;
+
     void Start(){
         shopToggle = GetComponent<Toggle>();
         shopToggle.onValueChanged.AddListener(delegate {
             ToggleValueChanged(shopToggle);
+            shopToggle.interactable = false;
+            Invoke("ResetCooldown", 0.5f);
         });
     }
+
+    void ResetCooldown(){
+        shopToggle.interactable = true;
+    }
+
     void ToggleValueChanged(Toggle change)
     {
         InventoryGameObj.GetComponent<Inventory>().PopulateStructuresTab();
