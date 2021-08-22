99c433c6304f4292e6f075c93abff8d42910311f
jamsge <james.ge1@outlook.com>
2021-08-21 20:45:29 -0700

    unimplemented clock visuals

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -5,19 +5,11 @@ using UnityEngine.UI;
 
 public class Clock : MonoBehaviour
 {
-    [SerializeField] private Text NightCount;
-    [SerializeField] private Text NightSeconds;
-    [SerializeField] private Image PieClock;
     [SerializeField] private int secondsInNight;
     private int totalElapsedSeconds = 0;
-    private int seconds;
-    private int night = 1;
-
     // Start is called before the first frame update
     void Start()
     {
-        seconds = secondsInNight;
-        NightCount.text = "Night " + night;
         InvokeRepeating("count", 0f, 1f);
     }
 
@@ -25,14 +17,5 @@ public class Clock : MonoBehaviour
     // TODO: move UI related code to different script
     private void count(){
         totalElapsedSeconds++;
-        seconds--;
-        if (seconds == 0){
-            seconds = secondsInNight;
-            night++;
-            NightCount.text = "Night " + night;
-            print("The night has passed.");
-        }
-        NightSeconds.text = "" + seconds;
-        PieClock.fillAmount = (float)seconds/(float)secondsInNight;
     }
 }
